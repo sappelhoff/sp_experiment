205b2e4d5f1a8734b7cd72f6115014a0eb16b8af
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2018-09-22 11:44:51 +0200

    save payoff distributions in their own module, to be easily imported

---
+++ sp_psychopy/payoff_distributions.py
@@ -0,0 +1,6 @@
+"""Provide the payoff distributions to be used in the Sampling Paradigm."""
+
+# Payoff 1
+payoff_dict_1 = {}
+payoff_dict_1[0] = [0] * 7 + [1] * 3
+payoff_dict_1[0] = [0] * 3 + [1] * 7
