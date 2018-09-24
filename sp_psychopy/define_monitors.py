fe676ebffe25fd0cee417e5d2e1d0956b0e6c813
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2018-09-24 17:49:31 +0200

    restructure and add doc

--- sp_psychopy/define_monitors.py
+++ sp_psychopy/define_monitors.py
@@ -1,13 +1,18 @@
-"""Define monitors."""
+"""Define monitors to be used.
+
+This is important for psychopy to work in units of visual degrees.
+"""
 from psychopy import monitors
 
-# All available monitors
-print(monitors.getAllMonitors())
-
 # Define new monitors
+# -------------------
+
 # My Lenovo P51 monitor
 my_monitor = monitors.Monitor(name='p51')
 my_monitor.setSizePix((1920, 1080))
 my_monitor.setWidth(34.6)  # width of display in cm
 my_monitor.setDistance(50)  # distance of eyes from screen in cm
 my_monitor.saveMon()
+
+# Print all available monitors
+print(monitors.getAllMonitors())
