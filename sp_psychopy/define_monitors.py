013ca0525d698575ff63e6f57826e2da96085db8
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2018-12-09 09:06:49 +0100

    define monitors upon main call of script

--- sp_psychopy/define_monitors.py
+++ sp_psychopy/define_monitors.py
@@ -6,24 +6,25 @@ This is important for psychopy to work in units of visual degrees.
 """
 from psychopy import monitors
 
-# Define new monitors
-# -------------------
+if __name__ == '__main__':
+    # Define new monitors
+    # -------------------
 
-# My Lenovo P51 monitor
-my_monitor = monitors.Monitor(name='p51')
-my_monitor.setSizePix((1920, 1080))
-my_monitor.setWidth(34.6)  # width of display in cm
-my_monitor.setDistance(50)  # distance of eyes from screen in cm
-my_monitor.saveMon()
+    # My Lenovo P51 monitor
+    my_monitor = monitors.Monitor(name='p51')
+    my_monitor.setSizePix((1920, 1080))
+    my_monitor.setWidth(34.6)  # width of display in cm
+    my_monitor.setDistance(50)  # distance of eyes from screen in cm
+    my_monitor.saveMon()
 
-# My Dell Latitude 7490 monitor
-my_monitor = monitors.Monitor(name='latitude7490')
-my_monitor.setSizePix((1920, 1080))
-my_monitor.setWidth(30)
-my_monitor.setDistance(50)  # distance of eyes from screen in cm
-my_monitor.saveMon()
+    # My Dell Latitude 7490 monitor
+    my_monitor = monitors.Monitor(name='latitude7490')
+    my_monitor.setSizePix((1920, 1080))
+    my_monitor.setWidth(30)
+    my_monitor.setDistance(50)  # distance of eyes from screen in cm
+    my_monitor.saveMon()
 
-# Add new monitor below
+    # Add new monitor below
 
-# Print all available monitors
-print(monitors.getAllMonitors())
+    # Print all available monitors
+    print(monitors.getAllMonitors())
