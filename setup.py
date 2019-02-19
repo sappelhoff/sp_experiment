d2f3a694b12398489f161bf1ffdf6f9a90ec45ef
Michael Krause <krause@mpib-berlin.mpg.de>
2019-02-19 18:13:10 +0100

    setup.py add deps, minimal classifiers, and json extra data

--- setup.py
+++ setup.py
@@ -12,6 +12,24 @@ with open(os.path.join('sp_experiment', '__init__.py'), 'r') as fid:
 if version is None:
     raise RuntimeError('Could not determine version')
 
+dependencies = [
+    'numpy',
+    'scipy',
+    'matplotlib',
+    'pandas',
+    'pyopengl',
+    'pyglet~=1.3.0',
+    'pillow',
+    'moviepy',
+    'lxml',
+    'openpyxl',
+    'configobj',
+    'psychopy==3.0.0',
+    'pytest',
+    'pytest-cov',
+]
+
+
 setup(name='sp_experiment',
       version=version,
       description='Implemetation of the Sampling Paradigm in PsychoPy',
@@ -19,5 +37,11 @@ setup(name='sp_experiment',
       author='Stefan Appelhoff',
       author_email='stefan.appelhoff@mailbox.org',
       license='BSD 3-Clause License',
+      classifiers=[
+          'Programming Language :: Python :: 3.6',
+          'Operating System :: Microsoft :: Windows',
+      ],
       packages=find_packages(),
+      install_requires=dependencies,
+      package_data={'': ['*.json']},
       zip_safe=False)
