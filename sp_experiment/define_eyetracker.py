correct gazepoint and allow conversion to psychopy norm units

sp_experiment/define_eyetracker.py
@@ -8,11 +8,12 @@ import os
 import csv
 from functools import partial
 
+import numpy as np
 import tobii_research as tr
 
 # global gaze_dict allows us to share the gazepoint of the left and right eye
-# as a tuple. By default it's set to be at (0, 0)
-gaze_dict = {'gaze': (0, 0)}
+# as tuples. By default it's set to be at (0, 0) for each eye
+gaze_dict = {'gaze': ((0, 0), (0, 0))}
 
 
 def find_eyetracker():
@@ -29,6 +30,33 @@ def find_eyetracker():
     print('Serial number: ' + eyetracker.serial_number)
     print('Sampling freq: {}Hz'.format(eyetracker.get_gaze_output_frequency()))
     return eyetracker
+
+
+def convert_gazepoint(gaze_dict):
+    """Convert Tobii system of two eyes to single normed gaze point.
+
+    Parameters
+    ----------
+    gaze : dict
+        Dictionary with key 'gaze' pointing to a value 'gaze', which is a
+        tuple of the left and right eye gaze points in Tobii coordinates.
+
+    Returns
+    -------
+    gazepoint : ndarray
+        X and Y coordinates of the gazepoint in a psychopy window with units
+        of type 'norm'
+
+    """
+    left = np.asarray(gaze_dict['gaze'][0])
+    right = np.asarray(gaze_dict['gaze'][1])
+    # Take mean of left and right eye
+    __ = np.array((left, right)).mean(axis=0)
+    # Reverse y axis
+    __[1] = 1 - __[1]
+    # Scale to range -1 1 for psychopy window unit "norm"
+    gazepoint = np.interp(__, (0, 1), (-1, 1))
+    return gazepoint
 
 
 def get_gaze_data_callback(fout_name):

sp_experiment/tests/test_define_eyetracker.py
@@ -1,13 +1,15 @@
 """Test eyetracker use."""
 import os
 import pytest
+import ast
 
 import numpy as np
 import pandas as pd
 
 from sp_experiment.define_eyetracker import (find_eyetracker,
                                              get_gaze_data_callback,
-                                             gaze_dict)
+                                             gaze_dict,
+                                             convert_gazepoint)
 
 
 def test_find_eyetracker():
@@ -17,42 +19,75 @@ def test_find_eyetracker():
         find_eyetracker()
 
 
+def test_convert_gazepoint():
+    """Test gazepoint conversion."""
+    # Tobii has a system with (0, 0) in upper left
+    gaze_dict = {'gaze': ((0, 0), (0, 0))}
+    gazepoint = convert_gazepoint(gaze_dict)
+    np.testing.assert_array_equal(gazepoint, np.array((-1, 1)))
+
+    gaze_dict = {'gaze': ((0, 1), (0, 1))}
+    gazepoint = convert_gazepoint(gaze_dict)
+    np.testing.assert_array_equal(gazepoint, np.array((-1, -1)))
+
+    gaze_dict = {'gaze': ((1, 1), (1, 1))}
+    gazepoint = convert_gazepoint(gaze_dict)
+    np.testing.assert_array_equal(gazepoint, np.array((1, -1)))
+
+    gaze_dict = {'gaze': ((1, 0), (1, 0))}
+    gazepoint = convert_gazepoint(gaze_dict)
+    np.testing.assert_array_equal(gazepoint, np.array((1, 1)))
+
+
 def test_get_gaze_data_callback():
     """Test the logging and making gaze_data globally available."""
     # make temp file with a hash so that it does probably not exist
     fname = 'tmp_ba0a6dd03443308b2ef5caa84ed30726fc2e368b.tsv'
 
     # Check the initial gaze
-    assert gaze_dict['gaze'][0] == 0
-    assert gaze_dict['gaze'][1] == 0
+    assert gaze_dict['gaze'][0][0] == 0
+    assert gaze_dict['gaze'][0][1] == 0
+    assert gaze_dict['gaze'][1][0] == 0
+    assert gaze_dict['gaze'][1][1] == 0
 
     # Make our callback function
     gaze_data_callback = get_gaze_data_callback(fname)
 
     # Pretend providing some gaze_data
     # this is usually provided from the eyetracker that is subscribed to
-    gaze_data = {'left_gaze_point_on_display_area': 0.3,
-                 'right_gaze_point_on_display_area': 0.7}
+    gaze_data = {'left_gaze_point_on_display_area': (0.3, 0),
+                 'right_gaze_point_on_display_area': (0.7, 0)}
     gaze_data_callback(gaze_data)
 
     # It should have updated our global gaze_dict
-    assert gaze_dict['gaze'][0] == 0.3
-    assert gaze_dict['gaze'][1] == 0.7
+    assert gaze_dict['gaze'][0][0] == 0.3
+    assert gaze_dict['gaze'][1][0] == 0.7
+    assert gaze_dict['gaze'][0][1] == 0
+    assert gaze_dict['gaze'][1][1] == 0
 
     # Try to update again
-    gaze_data = {'left_gaze_point_on_display_area': 0.4,
-                 'right_gaze_point_on_display_area': 0.6}
+    gaze_data = {'left_gaze_point_on_display_area': (0, 0.4),
+                 'right_gaze_point_on_display_area': (0, 0.6)}
     gaze_data_callback(gaze_data)
 
-    assert gaze_dict['gaze'][0] == 0.4
-    assert gaze_dict['gaze'][1] == 0.6
+    assert gaze_dict['gaze'][0][0] == 0
+    assert gaze_dict['gaze'][1][0] == 0
+    assert gaze_dict['gaze'][0][1] == 0.4
+    assert gaze_dict['gaze'][1][1] == 0.6
 
     # Check that logging to a file worked as well
     df = pd.read_csv(fname, sep='\t')
-    arr_left = df['left_gaze_point_on_display_area'].to_numpy()
-    arr_right = df['right_gaze_point_on_display_area'].to_numpy()
-    np.testing.assert_array_equal(arr_left, np.array((0.3, 0.4)))
-    np.testing.assert_array_equal(arr_right, np.array((0.7, 0.6)))
+
+    # We need to format the strings of tuples to an appropriate format
+    arr_left_bad_fmt = df['left_gaze_point_on_display_area'].to_numpy()
+    # ast.literal_eval is safe: https://stackoverflow.com/a/21735907/5201771
+    arr_left = np.asarray([ast.literal_eval(i) for i in arr_left_bad_fmt])
+
+    arr_right_bad_fmt = df['right_gaze_point_on_display_area'].to_numpy()
+    arr_right = np.asarray([ast.literal_eval(i) for i in arr_right_bad_fmt])
+
+    np.testing.assert_array_equal(arr_left, np.array(((0.3, 0), (0, 0.4))))
+    np.testing.assert_array_equal(arr_right, np.array(((0.7, 0), (0, 0.6))))
 
     # clean up
     os.remove(fname)