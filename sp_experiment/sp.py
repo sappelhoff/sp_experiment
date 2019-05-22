a4489104c8106af1aaf9b88e724e1d11206e9e55
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2019-05-22 13:56:16 +0200

    make circle around fixstim at start of trial irrespective of eyetrack

--- sp_experiment/sp.py
+++ sp_experiment/sp.py
@@ -438,33 +438,37 @@ def run_flow(monitor='testMonitor', ser=Fake_serial(), max_ntrls=10,
                      payoff_dict=payoff_dict)
 
         # Starting a new trial
+        # If we do eyetracking, make gaze-contingent fixation stim
+        # That is: Draw a circle around the fixation stim and continuously
+        # shrink the radius of the circle as long as the gaze rests in some
+        # tolerance distance of the fixation stim. Once the circle radius
+        # is the same size as the fixation stim, consider the gaze
+        # "captured" by the fication stim and move on. Whenever the gaze
+        # wanders elsewhere, reset the radius of the outer circle and start
+        # afresh
+        # if we DO NOT do eyetracking, we just shrink the circle irrespective
+        # of the gaze
+        progress = 0  # counter for tracking progress
+        tolerance_norm = 0.5  # how close to fix_stim must the gaze be?
+        radius_outer_deg = 5  # arbitrarily picked
+        radius_inner_deg = 0.6  # radius of fix_stim
+        circle = visual.Circle(win, units='deg', radius=radius_outer_deg,
+                               edges=128)
+
         [stim.setAutoDraw(True) for stim in fixation_stim_parts]
         set_fixstim_color(inner, color_newtrl)
         win.callOnFlip(ser.write, trig_dict['trig_new_trl'])
         frames = get_jittered_waitframes(*tdisplay_ms)
         for frame in range(frames):
+            circle.draw()
             win.flip()
             if frame == 0:
                 log_data(data_file, onset=exp_timer.getTime(),
                          trial=current_ntrls,
                          value=trig_dict['trig_new_trl'], duration=frames)
-            # If we do eyetracking, make gaze-contingent fixation stim
-            # That is: Draw a circle around the fixation stim and continuously
-            # shrink the radius of the circle as long as the gaze rests in some
-            # tolerance distance of the fixation stim. Once the circle radius
-            # is the same size as the fixation stim, consider the gaze
-            # "captured" by the fication stim and move on. Whenever the gaze
-            # wanders elsewhere, reset the radius of the outer circle and start
-            # afresh
-            if track_eyes:
-                fix_duration = frames - 1  # for how long to fixate on fix_stim
-                progress = 0  # counter for tracking progress
-                tolerance_norm = 0.5  # how close to fix_stim must the gaze be?
-                radius_outer_deg = 5  # arbitrarily picked
-                radius_inner_deg = 0.6  # radius of fix_stim
-                circle = visual.Circle(win, units='deg',
-                                       radius=radius_outer_deg, edges=128)
 
+            # Gaze contingent IF eyetracking
+            if track_eyes:
                 while True:
                     # get current gazepoint and calculate distance to center
                     gazepoint = get_normed_gazepoint(gaze_dict)
@@ -473,7 +477,7 @@ def run_flow(monitor='testMonitor', ser=Fake_serial(), max_ntrls=10,
                     # shrink focus circle until it merges with fix_stim
                     if dist_norm <= tolerance_norm:
                         progress += 1
-                        circle.setRadius(radius_outer_deg - ((radius_outer_deg - radius_inner_deg) / (fix_duration/progress)))  # noqa: E501
+                        circle.setRadius(radius_outer_deg - ((radius_outer_deg - radius_inner_deg) / (frames/progress)))  # noqa: E501
                     else:
                         # If gaze is too far off, reset the situation
                         circle.radius = radius_outer_deg
@@ -481,9 +485,14 @@ def run_flow(monitor='testMonitor', ser=Fake_serial(), max_ntrls=10,
 
                     circle.draw()
                     win.flip()
-                    if progress >= fix_duration:
+                    if progress >= frames:
                         break  # break from gaze-contingent fixstim
                 break  # break from non-track-eyes loop
+
+            # if NOT eyetracking, we still shrink a circle
+            else:
+                progress += 1
+                circle.setRadius(radius_outer_deg - ((radius_outer_deg - radius_inner_deg) / (frames/progress)))  # noqa: E501
 
         # Within this trial, allow sampling
         current_nsamples = 0
