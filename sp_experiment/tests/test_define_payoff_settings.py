5de5abd9dba06715921ed2d690f394eedd002e86
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2019-07-31 15:43:53 +0200

    add test

--- sp_experiment/tests/test_define_payoff_settings.py
+++ sp_experiment/tests/test_define_payoff_settings.py
@@ -8,6 +8,7 @@ import pandas as pd
 import sp_experiment
 from sp_experiment.define_payoff_settings import (get_payoff_settings,
                                                   get_random_payoff_dict,
+                                                  get_random_payoff_settings
                                                   )
 
 
@@ -61,3 +62,79 @@ def test_get_random_payoff_dict():
     assert payoff_dict3 == payoff_dict4
     payoff_dict5, _ = get_random_payoff_dict(payoff_settings, seed=2)
     assert payoff_dict4 != payoff_dict5
+
+
+def _simulate_run(rand_payoff_settings, n_samples= 12, seed=None):
+    """Simulate a participant with 50% 50% left right tendency."""
+    rng = np.random.RandomState(seed)
+    actions = list()
+    outcomes = list()
+    for setting in rand_payoff_settings:
+        payoff_dict = get_payoff_dict(setting)
+        for sample in range(n_samples):
+            action = rng.choice((0, 1))
+            actions.append(action)
+            outcome = rng.choice(payoff_dict[action])
+            outcomes.append(outcome)
+
+    actions = np.array(actions)
+    outcomes = np.array(outcomes)
+    # combine actions and outcomes to code outcomes on the left with negative
+    # sign outcomes on the right with positive sign ... will end up with stim
+    # classes: - sign for "left", + sign for "right"
+    stim_classes = outcomes * (actions*2-1)
+
+    return stim_classes
+
+
+def _make_class_hist(stim_classes):
+    """Turn stim_classes into hist."""
+    # Make a histogram of which stimulus_classes we have collected so far
+    bins = np.hstack((np.arange(-9, 0), np.arange(1, 11)))
+    stim_class_hist = np.histogram(stim_classes, bins)
+
+    # Make an array from the hist and sort it
+    stim_class_arr = np.vstack((stim_class_hist[0], stim_class_hist[1][:-1])).T
+    stim_class_arr_sorted = stim_class_arr[stim_class_arr[:, 0].argsort()]
+
+    return stim_class_arr_sorted
+
+
+def test_balancing():
+    """Test that we can get a balanced stimulus selection."""
+    seed = 1
+    max_ntrls = 100
+    ev_diff = 0.9
+    payoff_settings = get_payoff_settings(ev_diff)
+
+
+    # No balancing at all, this will lead to a few stim_classes never
+    # being shown
+    rand_payoff_settings = payoff_settings.copy()
+    rng = np.random.RandomState(seed)
+    perm = rng.permutation(max_ntrls)
+    rand_payoff_settings = rand_payoff_settings[perm, :]
+    stim_classes = _simulate_run(rand_payoff_settings, n_samples=12, seed=seed)
+    hist = _make_class_hist(stim_classes)
+    diff1 = np.diff(hist[[0, -1], 0])
+
+    # some balancing
+    rand_payoff_settings = get_random_payoff_settings(max_ntrls,
+                                                      payoff_settings,
+                                                      -1
+                                                      seed)
+    stim_classes = _simulate_run(rand_payoff_settings, n_samples=12, seed=seed)
+    hist = _make_class_hist(stim_classes)
+    diff2 = np.diff(hist[[0, -1], 0])
+
+    # Most balancing
+    rand_payoff_settings = get_random_payoff_settings(max_ntrls,
+                                                      payoff_settings,
+                                                      0.6
+                                                      seed)
+    stim_classes = _simulate_run(rand_payoff_settings, n_samples=12, seed=seed)
+    hist = _make_class_hist(stim_classes)
+    diff3 = np.diff(hist[[0, -1], 0])
+
+    assert diff1 > diff2
+    assert diff2 > diff3
